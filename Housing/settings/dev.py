5e8bbec570b7ae3b161b4e53eaff5f74006ee882
smoke <mohamedmura1995@gmail.com>
2022-02-13 17:33:06 +0200

    'mos'

--- Housing/settings/dev.py
+++ Housing/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['mos-real.herokuapp.com']
+ALLOWED_HOSTS = ['murra-realstate.herokuapp.com']
 SECRET_KEY = Config('SECRET_KEY')
 
 django_on_heroku.settings(locals(),staticfiles=False)
