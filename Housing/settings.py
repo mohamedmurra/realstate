6c497a1cf9547ca33208b23ac7828ec3c24c93e4
smoke <mohamedmura1995@gmail.com>
2022-02-13 10:44:42 +0200

    fk

--- Housing/settings.py
+++ Housing/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+import django_on_heroku
 
 
 
@@ -151,4 +151,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'static/media')
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
