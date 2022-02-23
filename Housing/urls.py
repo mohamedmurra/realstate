00c96402d859ddefc5ceeb64b167b56b89cd7252
smoke <mohamedmura1995@gmail.com>
2022-02-23 10:11:52 +0200

    'sd'

--- Housing/urls.py
+++ Housing/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Main.urls')),
     path('blog/', include('Blog.urls')),
-    path('tinymce/', include('tinymce.urls')),
 ]
 
 if settings.DEBUG:
