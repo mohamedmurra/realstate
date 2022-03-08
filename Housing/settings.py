ecabdab47810abd0a3f3030d1bc1dc734b59f62b
smoke <mohamedmura1995@gmail.com>
2022-03-08 22:33:17 +0200

    'fix'

--- Housing/settings.py
+++ Housing/settings.py
@@ -96,11 +96,8 @@ WSGI_APPLICATION = 'Housing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mohamedmurra$default',
-        'USER': 'mohamedmurra',
-        'PASSWORD': 'Mo111995@',
-        'HOST': 'mohamedmurra.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
