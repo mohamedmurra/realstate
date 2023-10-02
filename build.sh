2486b0dfb0489826096e50c92f117397dba2df78
mohamedmurra <99559770+mohamedmurra@users.noreply.github.com>
2023-10-02 05:23:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-python manage.py makemigrations
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install
+
+python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser
